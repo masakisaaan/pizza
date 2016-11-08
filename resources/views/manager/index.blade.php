356ea7b8682797545c6667f79bc00ab871ae06e1
namikaze <b5216@oic.jp>
2016-11-08 13:44:48 +0900

    従業員一覧作成(近澤)

---
+++ resources/views/manager/index.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>login</title>
+    <link rel="stylesheet" href="css/common/main.css" charset="utf-8">
+    <link rel="stylesheet" href="css/common/reset.css" charset="utf-8">
+    <link rel="stylesheet" href="css/common/bootstrap.min.css" charset="utf-8">
+    <link rel="stylesheet" href="css/pages/index.css" charset="utf-8">
+    <script src="js/common/bootstrap.min.js" charset="utf-8"></script>
+    </script>
+  </head>
+  <body id="">
+    <div class="wrap">
+    <h2>従業員管理画面</h2>
+    <a href="#"><input type="button" name="name" value="編集"></a>
+    <form class="" action="#" method="post">
+      <div class="container">
+  <table class="table">
+    <thead>
+      <tr>
+        <th></th>
+        <th>ID</th>
+        <th>氏名</th>
+        <th>フリガナ</th>
+        <th>生年月日</th>
+        <th>性別</th>
+        <th>契約開始日</th>
+        <th>契約終了日</th>
+        <th>登録日時</th>
+        <th>更新日時</th>
+        <th>住所</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row"><input type="checkbox" name="name" value=""></th>
+          <td>001</td>
+          <td>近澤</td>
+          <td>チカザワ</td>
+          <td>16600101</td>
+          <td>男</td>
+          <td>19991212</td>
+          <td>25101212</td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+          <td>煌木 次郎</td>
+          <td>jiro.kirameki@example.com</td>
+          <td>09033334444</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+          <td>煌木 花子</td>
+          <td>hanako.kirameki@example.com</td>
+          <td>09055556666</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+      <table>
+        <thead>
+          <th></th>
+          <th>ID</th>
+          <th>氏名</th>
+          <th>フリガナ</th>
+          <th>生年月日</th>
+          <th>性別</th>
+          <th>契約開始日</th>
+          <th>契約終了日</th>
+          <th>登録日時</th>
+          <th>更新日時</th>
+        </thead>
+        <tbody>
+          <td><input type="checkbox" name="name" value=""></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tbody>
+      </table>
+
+    </form>
+    </div>
+  </body>
+</html>
