25542b665ed6ec898e3d9e930540d9e26b883f3c
namikaze <b5216@oic.jp>
2016-11-11 16:19:54 +0900

    注文確認画面製作中(近澤)

---
+++ resources/views/itemconfirm/index.blade.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>login</title>
+    <link rel="stylesheet" href="css/common/main.css" charset="utf-8">
+    <link rel="stylesheet" href="css/common/reset.css" charset="utf-8">
+    <link rel="stylesheet" href="css/common/bootstrap.min.css" charset="utf-8">
+    <link rel="stylesheet" href="css/pages/index.css" charset="utf-8">
+    <!-- <script src="js/common/jQuery.js" charset="utf-8"></script> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+    <script src="js/common/bootstrap.min.js" charset="utf-8"></script>
+  </head>
+  <body id="">
+    <div class="wrap">
+    <h2>注文確認画面</h2>
+      <div class="container">
+        <h3>商品情報</h3>
+        <div class="table-responsive">
+  <table class="table"> <!-- サンプル -->
+    <thead class="menu_scrollHead">
+      <div class="row">
+      <tr>
+        <th style="width:">NO</th>
+        <th style="width:">商品名</th>
+        <th style="width:">ジャンル</th>
+        <th style="width:">価格</th>
+        <th style="width:">個数</th>
+      </tr>
+    </thead>
+    <tbody class="menu_scrollBody">
+      <tr>
+          <td>1</td>
+          <td>マルゲリータピザ</td>
+          <td>メイン</td>
+          <td>￥2525</td>
+          <td><select class="" name="number">
+      <?php for ($i=1; $i <=999 ; $i++) { ?>
+        <option value="<?php echo $i ?>"><?php echo $i ?></option>
+      <?php }?>
+    </select></td></tr>
+    <tr>
+        <td>1</td>
+        <td>マルゲリータピザ</td>
+        <td>メイン</td>
+        <td>￥2525</td>
+        <td><select class="" name="number">
+    <?php for ($i=1; $i <=999 ; $i++) { ?>
+      <option value="<?php echo $i ?>"><?php echo $i ?></option>
+    <?php }?>
+  </select></td></tr>
+  <tr>
+      <td>1</td>
+      <td>マルゲリータピザ</td>
+      <td>メイン</td>
+      <td>￥2525</td>
+      <td><select class="" name="number">
+  <?php for ($i=1; $i <=999 ; $i++) { ?>
+    <option value="<?php echo $i ?>"><?php echo $i ?></option>
+  <?php }?>
+</select></td></tr>
+<tr>
+    <td>1</td>
+    <td>マルゲリータピザ</td>
+    <td>メイン</td>
+    <td>￥2525</td>
+    <td><select class="" name="number">
+<?php for ($i=1; $i <=999 ; $i++) { ?>
+  <option value="<?php echo $i ?>"><?php echo $i ?></option>
+<?php }?>
+</select></td></tr>
+<tr>
+    <td>1</td>
+    <td>マルゲリータピザ</td>
+    <td>メイン</td>
+    <td>￥2525</td>
+    <td><select class="" name="number">
+<?php for ($i=1; $i <=999 ; $i++) { ?>
+  <option value="<?php echo $i ?>"><?php echo $i ?></option>
+<?php }?>
+</select></td></tr>
+<tr>
+  <td>1</td>
+  <td>マルゲリータピザ</td>
+  <td>メイン</td>
+  <td>￥2525</td>
+  <td><select class="" name="number">
+<?php for ($i=1; $i <=999 ; $i++) { ?>
+<option value="<?php echo $i ?>"><?php echo $i ?></option>
+<?php }?>
+</select></td></tr>
+<tr>
+<td>1</td>
+<td>マルゲリータピザ</td>
+<td>メイン</td>
+<td>￥2525</td>
+<td><select class="" name="number">
+<?php for ($i=1; $i <=999 ; $i++) { ?>
+<option value="<?php echo $i ?>"><?php echo $i ?></option>
+<?php }?>
+</select></td></tr>
+<tr>
+<td>1</td>
+<td>マルゲリータピザ</td>
+<td>メイン</td>
+<td>￥2525</td>
+<td><select class="" name="number">
+<?php for ($i=1; $i <=999 ; $i++) { ?>
+<option value="<?php echo $i ?>"><?php echo $i ?></option>
+<?php }?>
+</select></td></tr>
+<tr>
+    <td>1</td>
+    <td>マルゲリータピザ</td>
+    <td>メイン</td>
+    <td>￥2525</td>
+    <td><select class="" name="number">
+<?php for ($i=1; $i <=999 ; $i++) { ?>
+  <option value="<?php echo $i ?>"><?php echo $i ?></option>
+<?php }?>
+</select></td></tr>
+<tr>
+  <td>1</td>
+  <td>マルゲリータピザ</td>
+  <td>メイン</td>
+  <td>￥2525</td>
+  <td><select class="" name="number">
+<?php for ($i=1; $i <=999 ; $i++) { ?>
+<option value="<?php echo $i ?>"><?php echo $i ?></option>
+<?php }?>
+</select></td></tr>
+<tr>
+<td>1</td>
+<td>マルゲリータピザ</td>
+<td>メイン</td>
+<td>￥2525</td>
+<td><select class="" name="number">
+<?php for ($i=1; $i <=999 ; $i++) { ?>
+<option value="<?php echo $i ?>"><?php echo $i ?></option>
+<?php }?>
+</select></td></tr>
+<tr>
+<td>1</td>
+<td>マルゲリータピザ</td>
+<td>メイン</td>
+<td>￥2525</td>
+<td><select class="" name="number">
+<?php for ($i=1; $i <=999 ; $i++) { ?>
+<option value="<?php echo $i ?>"><?php echo $i ?></option>
+<?php }?>
+</select></td></tr>
+    </tbody>
+  </div>
+  </table>
+  <h3>顧客情報</h3>
+
+
+
+<div class="text-right">
+  <a href="#"><input type="button" class="btn btn-primary btn-lg" name="name" value="戻る"></a>
+  <a href="#"><input type="button" class="btn btn-primary btn-lg" name="name" value="決定"></a>
+</div>
+    </div>
+  </div>
+    </div>
+  </body>
+</html>
