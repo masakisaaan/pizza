84bf1fbe4ae1f2f95fa000374aa8a1577edf6d9a
namikaze <b5216@oic.jp>
2016-11-11 14:15:55 +0900

    従業員編集画面を作成暫定

--- resources/views/manageredit/index.blade.php
+++ resources/views/manageredit/index.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="css/common/bootstrap.min.css" charset="utf-8">
     <link rel="stylesheet" href="css/pages/index.css" charset="utf-8">
     <!-- <script src="js/common/jQuery.js" charset="utf-8"></script> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
     <script src="js/common/bootstrap.min.js" charset="utf-8"></script>
@@ -16,69 +17,129 @@
     <div class="wrap">
     <h3>従業員編集画面</h3>
       <div class="container">
+        <div class="table-responsive">
   <table class="table"> <!-- サンプル -->
     <thead>
+      <div class="row">
       <tr>
-        <th></th>
-        <th>ID</th>
-        <th>氏名</th>
-        <th>フリガナ</th>
-        <th>生年月日</th>
-        <th>性別</th>
-        <th>契約開始日</th>
-        <th>契約終了日</th>
-        <th>登録日時</th>
-        <th>更新日時</th>
-        <th>住所</th>
+        <th style="width:6%">ID</th>
+        <th style="width:11%">氏名</th>
+        <th style="width:14%">フリガナ</th>
+        <th style="width:3%">生年月日</th>
+        <th style="width:4%">性別</th>
+        <th style="width:7%">契約開始日</th>
+        <th style="width:7%">契約終了日</th>
+        <th style="width:4%">登録日時</th>
+        <th style="width:4%">更新日時</th>
+        <th style="width:21%">住所</th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <th scope="row"><input type="checkbox" name="name" value=""></th>
-          <td>001</td>
-          <td>近澤</td>
-          <td>チカザワ</td>
+          <td><input type="text" class="form-control" value="001"/></td>
+          <td><input type="text" class="form-control" value="近澤"/></td>
+          <td><input type="text" class="form-control" value="チカザワ"/></td>
           <td>16600101</td>
           <td>M</td>
           <td>19991212</td>
           <td>25101212</td>
           <td>00000000</td>
           <td>00000000</td>
-          <td>aaaaaaaaaaaaaaaaaaaaa</td>
+          <td><input type="text" class="form-control" value="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"/></td>
       </tr>
       <tr>
-        <th scope="row"><input type="checkbox" name="name" value=""></th>
-          <td>001</td>
-          <td>近澤</td>
-          <td>チカザワ</td>
-          <td>16600101</td>
-          <td>M</td>
-          <td>19991212</td>
-          <td>25101212</td>
-          <td>00000000</td>
-          <td>00000000</td>
-          <td>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</td>
+        <td><input type="text" class="form-control" value="001"/></td>
+        <td><input type="text" class="form-control" value="近澤"/></td>
+        <td><input type="text" class="form-control" value="チカザワ"/></td>
+        <td>16600101</td>
+        <td>M</td>
+        <td>19991212</td>
+        <td>25101212</td>
+        <td>00000000</td>
+        <td>00000000</td>
+        <td><input type="text" class="form-control" value="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"/></td>
       </tr>
       <tr>
-        <th scope="row"><input type="checkbox" name="name" value=""></th>
-          <td>001</td>
-          <td>近澤</td>
-          <td>チカザワ</td>
+          <td><input type="text" class="form-control" value="001"/></td>
+          <td><input type="text" class="form-control" value="近澤"/></td>
+          <td><input type="text" class="form-control" value="チカザワ"/></td>
           <td>16600101</td>
           <td>M</td>
           <td>19991212</td>
           <td>25101212</td>
           <td>00000000</td>
           <td>00000000</td>
-          <td>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</td>
+          <td><input type="text" class="form-control" value="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"/></td>
+      </tr>
+      <tr>
+        <td><input type="text" class="form-control" value="001"/></td>
+        <td><input type="text" class="form-control" value="近澤"/></td>
+        <td><input type="text" class="form-control" value="チカザワ"/></td>
+        <td>16600101</td>
+        <td>M</td>
+        <td>19991212</td>
+        <td>25101212</td>
+        <td>00000000</td>
+        <td>00000000</td>
+        <td><input type="text" class="form-control" value="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"/></td>
+      </tr>
+      <tr>
+          <td><input type="text" class="form-control" value="001"/></td>
+          <td><input type="text" class="form-control" value="近澤"/></td>
+          <td><input type="text" class="form-control" value="チカザワ"/></td>
+          <td>16600101</td>
+          <td>M</td>
+          <td>19991212</td>
+          <td>25101212</td>
+          <td>00000000</td>
+          <td>00000000</td>
+          <td><input type="text" class="form-control" value="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"/></td>
+      </tr>
+      <tr>
+        <td><input type="text" class="form-control" value="001"/></td>
+        <td><input type="text" class="form-control" value="近澤"/></td>
+        <td><input type="text" class="form-control" value="チカザワ"/></td>
+        <td>16600101</td>
+        <td>M</td>
+        <td>19991212</td>
+        <td>25101212</td>
+        <td>00000000</td>
+        <td>00000000</td>
+        <td><input type="text" class="form-control" value="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"/></td>
+      </tr>
+      <tr>
+          <td><input type="text" class="form-control" value="001"/></td>
+          <td><input type="text" class="form-control" value="近澤"/></td>
+          <td><input type="text" class="form-control" value="チカザワ"/></td>
+          <td>16600101</td>
+          <td>M</td>
+          <td>19991212</td>
+          <td>25101212</td>
+          <td>00000000</td>
+          <td>00000000</td>
+          <td><input type="text" class="form-control" value="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"/></td>
+      </tr>
+      <tr>
+        <td><input type="text" class="form-control" value="001"/></td>
+        <td><input type="text" class="form-control" value="近澤"/></td>
+        <td><input type="text" class="form-control" value="チカザワ"/></td>
+        <td>16600101</td>
+        <td>M</td>
+        <td>19991212</td>
+        <td>25101212</td>
+        <td>00000000</td>
+        <td>00000000</td>
+        <td><input type="text" class="form-control" value="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"/></td>
       </tr>
     </tbody>
+  </div>
   </table>
-</div>
-<div id="manager_button">
+<div class="text-right">
   <a href="#"><input type="button" class="btn btn-primary btn-lg" name="name" value="戻る"></a>
   <a href="#"><input type="button" class="btn btn-primary btn-lg" name="name" value="決定"></a>
 </div>
+    </div>
+  </div>
     </div>
   </body>
 </html>
