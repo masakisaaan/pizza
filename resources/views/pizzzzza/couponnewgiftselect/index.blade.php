7907f89b6b761ff6eeb8cd0b89a618e49e889da3
namikaze <b5216@oic.jp>
2016-11-15 14:00:46 +0900

    新規クーポン商品選択ファイル作成

---
+++ resources/views/pizzzzza/couponnewgiftselect/index.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>login</title>
+    <link rel="stylesheet" href="css/common/main.css" charset="utf-8">
+    <link rel="stylesheet" href="css/common/reset.css" charset="utf-8">
+    <link rel="stylesheet" href="css/common/bootstrap.min.css" charset="utf-8">
+    <link rel="stylesheet" href="css/pages/index.css" charset="utf-8">
+    <!-- <script src="js/common/jQuery.js" charset="utf-8"></script> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+    <script src="js/common/bootstrap.min.js" charset="utf-8"></script>
+  </head>
+  <body id="">
+    <div class="wrap">
+      <div class="">
+    <h3>従業員編集画面</h3>
+    <div class="text-right">
+      <a href="#"><input type="button" class="btn btn-primary btn-lg" name="name" value="戻る"></a>
+      <a href="#"><input type="button" class="btn btn-primary btn-lg" name="name" value="決定"></a>
+    </div>
+  </div>
+      <div class="container">
+  <table class="table"> <!-- サンプル -->
+      <div class="row">
+
+  </div>
+  </table>
+
+    </div>
+  </div>
+  </body>
+</html>
